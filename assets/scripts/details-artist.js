d757025f00c9a3630000fdb07e0acb504f971c83
leminhkientuong <leminhkientuong@gmail.com>
2026-02-01 18:02:28 +0700

    init

--- assets/scripts/details-artist.js
+++ assets/scripts/details-artist.js
@@ -3,7 +3,7 @@
 // ========================================
 const API_CONFIG = {
     host: 'genius-song-lyrics1.p.rapidapi.com',
-    key: '3dc5636b7amsh5e270d52d86cf8ap1509d6jsn2aacc81b2040',
+    key: '65ecc62e44msh552bb8e1370b20bp1bc025jsne56e1cfa3325',
     baseURL: 'https://genius-song-lyrics1.p.rapidapi.com'
 };
 
